1ad2872a132b80cf61132bd32aab9eb0f397805a
sean2525 <madness48596@gmail.com>
2018-11-20 16:33:08 +0800

    Connect NLP 	modified:   app/modules/svm/__init__.py 	modified:   app/sockets/speech.py 	modified:   app/utils/wav.py

--- app/modules/svm/__init__.py
+++ app/modules/svm/__init__.py
@@ -1,29 +1,24 @@
 import logging
 
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from imblearn.under_sampling import NearMiss
 from sklearn.pipeline import Pipeline
 import numpy as np
 import pickle
 
 LOG = logging.getLogger(__name__)
+
 X_sample = np.load('app/modules/svm/data.npy')
-#y_sample = np.load('app/modules/svm/label.npy')
+y_sample = np.load('app/modules/svm/label.npy')
 
 def build_svm_clf(ivectors, fbid):
     pipe_svm = Pipeline([('sc', StandardScaler()),
-                        ('clf', SVC(kernel='linear', C=1.0, random_state=0,probability=True))])
+                        ('clf', SVC(kernel='linear', C=1.0, probability=True))])
 
     X = np.concatenate((np.array(ivectors), X_sample))
     y = np.concatenate(([True]*len(ivectors), [False]*len(X_sample)))
 
-    nm2 = NearMiss(version=2)
-    X_train, y_train = nm2.fit_resample(X, y)
-
-
-    pipe_svm.fit(X_train, y_train)
+    pipe_svm.fit(X, y)
 
     LOG.info('{} clf score: {}'.format(fbid, pipe_svm.score(X, y)))
 
--- app/sockets/speech.py
+++ app/sockets/speech.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 
 from app import sio
 from app.modules.db.mongo import Users
@@ -20,7 +21,7 @@ class SpeechWebsocket(socketio.AsyncNamespace):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.streamer = {}
-        self.google = GoogleStreamer()
+        self.google = GoogleStreamer(rate=44100)
 
     def sanic_auth(func):
         @wraps(func)
@@ -70,25 +71,35 @@ class SpeechWebsocket(socketio.AsyncNamespace):
             del self.streamer[sid]['stream']
 
         if self.streamer[sid].get('responses'):
-            #self.streamer[sid]['responses'].cancel()
+            self.streamer[sid]['responses'].cancel()
             del self.streamer[sid]['responses']
             proba = 0
+            result = {
+                    'text': '',
+                    'url': ''
+                    }
+
             if self.streamer[sid].get('clf'):
                 try:
-                    wav = byte2wav(self.streamer[sid]['buff'])
+                    wav = byte2wav(self.streamer[sid]['buff'], 44100)
                     del self.streamer[sid]['buff']
                     ivector = ivector_pipeline(wav , sid)
-                    proba = self.streamer[sid]['clf'].predict_proba(ivector.reshape(1, -1))[0][0]
-                    print(proba)
-                    LOG.info(self.streamer[sid]['clf'].predict(ivector.reshape(1, -1)))
+                    clf = self.streamer[sid]['clf']
+                    if clf.predict(ivector.reshape(1, -1))[0]:
+                        result = requests.get('http://140.125.45.147:8080/get?speech={}'.format(self.streamer[sid]['google'])).json()
+
+                    proba = clf.predict_proba(ivector.reshape(1, -1))
+                    LOG.debug(proba)
+                    proba = proba[0][1]
+
                 except Exception as err:
-                    raise err
                     LOG.error(err)
 
             data = {
                     'proba': proba,
-                    'result': 'test'
+                    'result': result
                     }
+
             await self.emit('stop_stream', data, room=sid)
             return
 
@@ -101,11 +112,10 @@ class SpeechWebsocket(socketio.AsyncNamespace):
 
         if not self.streamer[sid].get('responses'):
             stream = self.streamer[sid]['stream']
-            responses = {'test'}
-            #responses = self.google.start_recognition_stream(stream.generator())
+            responses = self.google.start_recognition_stream(stream.generator())
             self.streamer[sid]['responses'] = responses
-            #thread_run_until_complete(self.handle_google_response(sid, responses))
-            #LOG.info('{} start google recognition'.format(sid))
+            thread_run_until_complete(self.handle_google_response(sid, responses))
+            LOG.info('{} start google recognition'.format(sid))
 
         self.streamer[sid]['stream'].write(data)
         self.streamer[sid]['buff'].write(data)
@@ -114,7 +124,6 @@ class SpeechWebsocket(socketio.AsyncNamespace):
     async def handle_google_response(self, sid, responses):
         try:
             for res in responses:
-
                 if not res.results:
                     continue
 
@@ -129,6 +138,7 @@ class SpeechWebsocket(socketio.AsyncNamespace):
                 if result.is_final:
                     data['is_final'] = True
 
+                self.streamer[sid]['google'] = transcript
                 LOG.debug(data)
                 await self.emit('google_speech_data', data, room=sid)
         except Exception as err:
